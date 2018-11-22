2d058cc376de276567fce67132d340b1d8d1af65
David Glassanos <david@naturebox.com>
2018-11-21 19:09:14 -0800

    Renaming new code to old code

--- Makefile
+++ Makefile
@@ -1,12 +1,8 @@
 
-.PHONY: mf mf2 clean
+.PHONY: mf clean
 
 mf: clean
-	gcc -o ./bin/mf src/mf.c -I src/ src/matrix.c src/array.c src/str.c
-
-mf2: clean
-	gcc -o ./bin/mf2 src2/mf2.c -I src2/ src2/matrix.c src2/str.c
+	gcc -o ./bin/mf src/mf.c -I src/ src/matrix.c src/str.c
 
 clean:
 	rm -rf ./bin/mf
-	rm -rf ./bin/mf2
--- src/matrix.c
+++ src/matrix.c
@@ -1,104 +1,31 @@
-#include <stdio.h>
+//
+// Created by David Glassanos on 11/21/18.
+//
+
 #include <stdlib.h>
-#include <math.h>
+#include <printf.h>
+#include "matrix.h"
 
-#include "array.h"
-
-/* frees a 2d array created by matrix_malloc */
-void matrix_free(void **a) {
-	void **row;
-
-	/* first free rows */
-	for(row = a; *row != 0; row++) {
-		free(*row);
+void freeMatrix(Matrix* matrix) {
+	// free array of cols for each row
+	for (int i = 0; i < matrix->rows; i++) {
+		free(matrix->data[i]);
 	}
 
-	/* then free array of rows */
-	free(a);
+	// then free array of rows
+	free(matrix->data);
+
+	// then free the matrix
+	free(matrix);
 }
 
-/* returns a two-dimensional array with numRows rows and
- * rowSize bytes per row, or 0 on allocation failure.
- * The caller is responsible for freeing the result with free2d. */
-void** matrix_malloc(size_t numRows, size_t rowSize)
-{
-	void **a;
-	size_t i;
+void printMatrix(Matrix* matrix) {
+	printf("rows %d\n", matrix->rows);
+	printf("cols %d\n", matrix->cols);
 
-	/* a is an array of void * pointers that point to the rows */
-	/* The last element is 0, so free2d can detect the last row */
-	a = malloc(sizeof(void *) * (numRows + 1));        /* one extra for sentinel */
-	if(a == 0) {
-		/* malloc failed */
-		return 0;
-	}
-
-	/* now allocate the actual rows */
-	for(i = 0; i < numRows; i++) {
-		a[i] = malloc(rowSize);
-		if(a[i] == 0) {
-			/* note that 0 in a[i] will stop freed2d after it frees previous rows */
-			matrix_free(a);
-			return 0;
-		}
-	}
-
-	/* initialize the sentinel value */
-	a[numRows] = 0;
-
-	return a;
-}
-
-void matrix_transpose(double **dest, double **src, int src_h, int src_w)
-{
-	int i, j;
-	for (i = 0; i < src_h; i++) {
-		for( j = 0 ; j < src_w ; j++ ) {
-			dest[j][i] = src[i][j];
-		}
-	}
-}
-
-void cross_product_row(double* dest, double** src, int row, int src_w)
-{
-	int i;
-	for (i = 0; i < src_w; i++) {
-		dest[i] = src[row][i];
-	}
-}
-
-void cross_product_col(double* dest, double** src, int col, int src_h)
-{
-	int j;
-	for (j = 0; j < src_h; j++) {
-		dest[j] = src[j][col];
-	}
-}
-
-void matrix_dot_product(double** dest, double** a, double** b, size_t k, size_t rowsize, size_t colsize)
-{
-	double row[rowsize];
-	double col[colsize];
-	int i, j;
-
-	for (i = 0; i < rowsize; i++) {
-		cross_product_row(row, a, i, k);
-
-		for (j = 0; j < colsize; j++) {
-			cross_product_col(col, b, j, k);
-
-			dest[i][j] = dot_product_array(row, col, k);
-		}
-	}
-}
-
-void matrix_print(char *str, double** p, int N, int M) {
-	printf("%s\n", str);
-	printf("------------------\n");
-	int i, j;
-	for(i = 0 ; i < N ; i++) {
-		for(j = 0 ; j < M ; j++) {
-			printf("%f ", p[i][j]);
+	for (int i = 0; i < matrix->rows; i++) {
+		for (int k = 0; k < matrix->cols; k++) {
+			printf("%f\t", matrix->data[i][k]);
 		}
 		printf("\n");
 	}
@@ -106,73 +33,25 @@ void matrix_print(char *str, double** p, int N, int M) {
 	printf("\n");
 }
 
-/**
- *
- * @param R the matrix to be factorized, dimension N x M
- * @param P an initial matrix of dimension N x K
- * @param Q an initial matrix of dimension M x K
- * @param N
- * @param M
- * @param K
- */
-void matrix_factorize(double** R, double** P, double** Q, double** rN, int N, int M, int K) {
-	int steps = 5000;
-	double alpha = 0.0002;
-	double beta = 0.02;
+void initMatrix(Matrix* matrix, int rows, int cols) {
+	matrix->rows = rows;
+	matrix->cols = cols;
 
-	double** QT = (double **) matrix_malloc(K, M * sizeof(double));
-	matrix_transpose(QT, Q, M, K);
-
-	int step, i, j, h;
-	for (step = 0; step < steps; step++) {
-		for (i = 0; i < N; i++) {
-			for (j = 0; j < M; j++) {
-				if (R[i][j] > 0) {
-					double row[K];
-					double col[K];
-					cross_product_row(row, P, i, K);
-					cross_product_col(col, QT, j, K);
-
-					// eij = R[i][j] - numpy.dot(P[i,:],Q[:,j])
-					double eij = R[i][j] - dot_product_array(row, col, K);
-
-					for (h = 0; h < K; h++) {
-						// P[i][h] = P[i][h] + alpha * (2 * eij * Q[h][j] - beta * P[i][h])
-						P[i][h] = P[i][h] + alpha * (2 * eij * QT[h][j] - beta * P[i][h]);
-
-						// Q[h][j] = Q[h][j] + alpha * (2 * eij * P[i][h] - beta * Q[h][j])
-						QT[h][j] = QT[h][j] + alpha * (2 * eij * P[i][h] - beta * QT[h][j]);
-					}
-				}
-			}
-		}
-
-		double e = 0.0;
-
-		for (i = 0; i < N; i++) {
-			for (j = 0; j < M; j++) {
-				if (R[i][j] > 0) {
-					double row[K];
-					double col[K];
-					cross_product_row(row, P, i, K);
-					cross_product_col(col, QT, j, K);
-					double eij = R[i][j] - dot_product_array(row, col, K);
-
-					e = e + pow(eij, 2);
-
-					for (h = 0; h < K; h++) {
-						e = e + (beta/2) * (pow(P[i][h], 2) + pow(QT[h][j], 2));
-					}
-				}
-			}
-		}
-
-		if (e < 0.001) {
-			break;
-		}
+	// matrix->data is an array of double * pointers that point to the rows
+	matrix->data = (double **) malloc(sizeof(double *) * matrix->rows);
+	if(matrix->data == 0) {
+		// malloc failed
+		return;
 	}
 
-	matrix_dot_product(rN, P, QT, K, N, M);
+	// now allocate the actual rows
+	for(int i = 0; i < matrix->rows; i++) {
+		matrix->data[i] = calloc((size_t)matrix->cols, sizeof(double*));
 
-	matrix_free((void**)QT);
+		// if malloc failed, free memory
+		if(matrix->data[i] == 0) {
+			freeMatrix(matrix);
+			return;
+		}
+	}
 }
